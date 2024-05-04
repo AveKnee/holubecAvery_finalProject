6a58f9667186e75a6de24d992609c19331536784
Yreva <enderholubec@gmail.com>
2024-05-04 15:24:32 -0500

    added proper main function

--- holubecAvery_final.py
+++ holubecAvery_final.py
@@ -110,100 +110,104 @@ def save_black_pixels(image):
     '''
     pygame.image.save(transparent_image, "Lineart.png")
 
-'''
-get image name from user input
-'''
-image_path = input("Enter the name of the image (ex: 'image.jpg'/'image.png'): ")
+def main():
+    '''
+    get image name from user input
+    '''
+    image_path = input("Enter the name of the image (ex: 'image.jpg'/'image.png'): ")
 
-try:
-    '''
-    open orignal image
-    '''
-    original_image = pygame.image.load(image_path)
+    try:
+        '''
+        open orignal image
+        '''
+        original_image = pygame.image.load(image_path)
+
+        '''
+        check if the image was properly opened
+        '''
+        if original_image is None:
+            raise ValueError("Unable to load image. Make sure the file name is correct.")
+    except pygame.error as e:
+        print("Error loading image:", e)
+        pygame.quit()
+        sys.exit()
+    except Exception as e:
+        print("An error occurred:", e)
+        pygame.quit()
+        sys.exit()
 
     '''
-    check if the image was properly opened
+    convert original image to monochrome
     '''
-    if original_image is None:
-        raise ValueError("Unable to load image. Make sure the file name is correct.")
-except pygame.error as e:
-    print("Error loading image:", e)
-    pygame.quit()
-    sys.exit()
-except Exception as e:
-    print("An error occurred:", e)
-    pygame.quit()
-    sys.exit()
+    monochrome_image = monochrome(original_image)
 
-'''
-convert original image to monochrome
-'''
-monochrome_image = monochrome(original_image)
-
-'''
-boost last picked color
-'''
-last_picked_color = (0, 0, 0)
-
-'''
-Main loop to update image
-'''
-while True:
     '''
-    checking for button presses and resizing
+    boost last picked color
     '''
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.VIDEORESIZE:
-            '''
-            call resize_display method if window width or height has changed
-            '''
-            resize_display(event.w, event.h)
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            '''
-            check to see if user has clicked on screen
-            then save the color and print it in the terminal
-            '''
-            if event.button == 1:
+    last_picked_color = (0, 0, 0)
+
+    '''
+    Main loop to update image
+    '''
+    while True:
+        '''
+        checking for button presses and resizing
+        '''
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.VIDEORESIZE:
                 '''
-                verify that mouse position is correct for window width and height 
+                call resize_display method if window width or height has changed
                 '''
-                adjusted_pos = (event.pos[0] - (screen_width - monochrome_image.get_width()) // 2, 
-                                event.pos[1] - (screen_height - monochrome_image.get_height()) // 2)
-                color_picked = pick_color(monochrome_image, adjusted_pos)
-                last_picked_color = color_picked
-                print("Color picked:", color_picked)
-        elif event.type == pygame.KEYDOWN:
-            '''
-            check to see if the 'b' key has been pressed and call the boost
-            if the 'b' key wasn't pressed, check to see if the 's' key was pressed
-            if the 's' key was pressed call the save function
-            '''
-            if event.key == pygame.K_b:
-                monochrome_image = boost_image(monochrome_image, last_picked_color)
-            elif event.key == pygame.K_s:
-                save_black_pixels(monochrome_image)
+                resize_display(event.w, event.h)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                '''
+                check to see if user has clicked on screen
+                then save the color and print it in the terminal
+                '''
+                if event.button == 1:
+                    '''
+                    verify that mouse position is correct for window width and height 
+                    '''
+                    adjusted_pos = (event.pos[0] - (screen_width - monochrome_image.get_width()) // 2, 
+                                    event.pos[1] - (screen_height - monochrome_image.get_height()) // 2)
+                    color_picked = pick_color(monochrome_image, adjusted_pos)
+                    last_picked_color = color_picked
+                    print("Color picked:", color_picked)
+            elif event.type == pygame.KEYDOWN:
+                '''
+                check to see if the 'b' key has been pressed and call the boost
+                if the 'b' key wasn't pressed, check to see if the 's' key was pressed
+                if the 's' key was pressed call the save function
+                '''
+                if event.key == pygame.K_b:
+                    monochrome_image = boost_image(monochrome_image, last_picked_color)
+                elif event.key == pygame.K_s:
+                    save_black_pixels(monochrome_image)
 
-    '''
-    make the background of the image viewer white
-    after the original image has been imported
-    '''
-    screen.fill((255, 255, 255))
+        '''
+        make the background of the image viewer white
+        after the original image has been imported
+        '''
+        screen.fill((255, 255, 255))
 
-    '''
-    calculate center of image viewer window to center imported image
-    '''
-    image_x = (screen_width - monochrome_image.get_width()) // 2
-    image_y = (screen_height - monochrome_image.get_height()) // 2
+        '''
+        calculate center of image viewer window to center imported image
+        '''
+        image_x = (screen_width - monochrome_image.get_width()) // 2
+        image_y = (screen_height - monochrome_image.get_height()) // 2
 
-    '''
-    display converted version of image
-    '''
-    screen.blit(monochrome_image, (image_x, image_y))
+        '''
+        display converted version of image
+        '''
+        screen.blit(monochrome_image, (image_x, image_y))
 
-    '''
-    update display after each loop
-    '''
-    pygame.display.update()
+        '''
+        update display after each loop
+        '''
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main()
